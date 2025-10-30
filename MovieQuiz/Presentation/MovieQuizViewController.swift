4a3ef12ef82b4f1defed2f3bc93cf11e5fa96699
Mikita Ivanou <ivanovnk30@gmail.com>
2025-10-30 21:50:56 +0300

    Работает загрузочный экран и вывод первого вопроса

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -4,7 +4,60 @@ final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        let currentQuestion = questions[currentQuestionIndex]
+        let quizStepModel = convert(model: currentQuestion)
+        show(quiz: quizStepModel)
     }
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма выше 8?", correctAnswer: true),
+        QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма выше 7?", correctAnswer: false),
+        QuizQuestion(image: "Old", text: "Рейтинг этого фильма выше 6?", correctAnswer: false),
+        QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма выше 7?", correctAnswer: true),
+        QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма выше 8?", correctAnswer: true),
+        QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма выше 9?", correctAnswer: true),
+        QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма ниже 7?", correctAnswer: false),
+        QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма выше 6?", correctAnswer: false),
+        QuizQuestion(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма выше 5?", correctAnswer: true),
+        QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма ниже 6?", correctAnswer: true)]
+   
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let image = UIImage(named: model.image) ?? UIImage()
+              let questionNumber = "\(currentQuestionIndex + 1)/\(questions.count)"
+        
+        return QuizStepViewModel(image: image,
+                                 question: model.text,
+                                 questionNumber: questionNumber)
+    }
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
+   
+    struct QuizStepViewModel {
+      let image: UIImage
+      let question: String
+      let questionNumber: String
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+      imageView.image = step.image
+      textLabel.text = step.question
+      counterLabel.text = step.questionNumber
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+    }
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private var imageView: UIImageView!
+    
 }
 
 /*
